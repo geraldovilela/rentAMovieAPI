065517f1cb5df725dcf4b0dbbfc946dd18119912
Geraldo Vilela <geraldovilela19@gmail.com>
2020-11-10 22:57:18 -0400

    dev: adjust for update data for movie route.

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -11,13 +11,13 @@ interface Movies {
 
 const movies: Movies[] = [];
 
-movieRouter.get('/', (request, response) => {
-  try {
-    return response.status(200).json(movies);
-  } catch (err) {
-    return response.status(500).json({ error: err.message });
-  }
-});
+movieRouter.get('/',  (request, response) => {
+    try {
+      return response.status(200).json(movies);
+    } catch (err) {
+      return response.status(500).json({ error: err.message });
+    }
+  });
 
 movieRouter.post('/', (request, response) => {
   const { title, director } = request.body;
@@ -33,15 +33,23 @@ movieRouter.post('/', (request, response) => {
   return response.status(200).json(newMovie);
 });
 
-movieRouter.put('/:id', (request, responde) => {
+movieRouter.put('/:id', (request, response) => {
   const { id } = request.params;
   const { title, director } = request.body;
 
   const movieIndex = movies.findIndex(movie => {
     if (movie.id === id) return movie;
   });
+  const oldMovie = movies[movieIndex];
+  if(movieIndex===-1){
+    return response.send({error:`invalid id ${id}`})
+  }
 
-  console.log(movieIndex);
+  oldMovie.director = (!director) ? oldMovie.director : director;
+  oldMovie.title = (!title)? oldMovie.title : title;
+
+  console.log(movies);
+  return response.status(200).json(oldMovie);
 });
 
 export default movieRouter;
